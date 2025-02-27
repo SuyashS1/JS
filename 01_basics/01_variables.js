8065f75e08422cfec579db4e0c5c683decdd2fa4
SuyashS1 <sikchisuyash1@gmail.com>
2025-02-27 13:27:15 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+var cityName="sonai"
+
+let stateName="Maharashtra"
+
+const districtName="Ahilya Nagar"
+
+var cityName="Pune"
+
+stateName="Gujrat"
+
+cityName="Sonai"
+
+area="Sikchi ln"
+
+stateName="Maharashtra"
+
+console.table([area, cityName, districtName, stateName])
