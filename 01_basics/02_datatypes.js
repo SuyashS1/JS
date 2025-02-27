99825707be54649ef7a5505a8f4c16d8248b9dfb
SuyashS1 <sikchisuyash1@gmail.com>
2025-02-27 13:27:32 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,33 @@
+// 7 primitive datatypes in JS
+
+//1. Number
+let roll_no=53
+
+//2. String
+let fName="Suyash"
+
+//3. Boolean
+let isTopper=false
+
+//4. BigInt
+let percentage=93.23243234234
+
+//5. Undefined
+let rank
+
+//6. Null
+let topper=null
+
+//7. Symbol
+
+console.table([roll_no, fName, isTopper, percentage, rank, topper])
+
+console.table(typeof roll_no)
+console.table(typeof fName)
+console.table(typeof isTopper)
+console.table(typeof percentage)
+console.table(typeof rank)
+console.table(typeof topper)
+
+
+
