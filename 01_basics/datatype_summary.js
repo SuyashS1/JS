1c20a244accb559f107a9adafe671ee577b1a6e2
SuyashS1 <sikchisuyash1@gmail.com>
2025-03-03 14:03:05 +0000

    summary of datatypes

---
+++ 01_basics/datatype_summary.js
@@ -0,0 +1,14 @@
+// Types of datatypes
+    //1.Primitive  --> passed by value, built-in datatypes
+    //    7 types:Number,String,Boolean,Undefined,BigInt,Null,Symbol
+
+
+    //2.Non-Primitive --> passed by reference, derived datatypes
+    //   3 types:Object,Array,Function
+
+ /*   passed by value --> the copy of orginal value is provided.(changes made in copy)
+
+    passed by reference --> the reference of orginal value is provided.
+
+
+*/ 
