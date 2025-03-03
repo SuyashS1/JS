44faaf34c3a77eae4390d1fe8c48b70a92242358
SuyashS1 <sikchisuyash1@gmail.com>
2025-03-03 12:59:33 +0000

    operations

---
+++ 01_basics/04_operations.js
@@ -0,0 +1,58 @@
+/*
+
+//1) Arthmetic operators
+let a=2
+let b=3
+
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+console.log(a%b)
+console.log(a**b)
+console.log(++a)
+console.log(--a)
+console.log(b++)
+console.log(b--)
+console.log(b)
+
+
+//2)Comparison operators
+let a="97"
+let b=97
+let c
+let d=null
+let e="a"
+
+console.log(a===b)
+console.log(a>=b)
+console.log(a<=b)
+console.log(c>=0)
+console.log(c<=0)
+console.log(a!=b)
+console.log(a===c)
+console.log(b===e)
+
+
+//3) Assignment operators
+let a=10
+let b=20
+
+console.log(a=b)
+console.log(a+=2)
+console.log(a-=2)
+console.log(a*=2)
+console.log(a/=2)
+console.log(a**=2)
+
+
+//4) Logic operators
+let a
+let b=20
+let c=30
+
+console.log(a<b && b<c && c<a)
+console.log(a<b || b<c || c<a)
+console.log(!a);
+    
+*/
