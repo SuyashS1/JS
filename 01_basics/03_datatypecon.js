4095548aab6dbea5786a0156f94d7ad2d2610b30
SuyashS1 <sikchisuyash1@gmail.com>
2025-03-03 12:05:13 +0000

    Datatype Conversion

---
+++ 01_basics/03_datatypecon.js
@@ -0,0 +1,38 @@
+
+// "33" ==> 33
+// "33abc" ==> NaN(not a number)
+// true ==> 1, false ==> 0, "" ==> false, "yash" ==> true.
+
+/*
+//1)number to string 
+
+let marks=25
+console.log(marks)
+console.log(typeof marks);
+
+let toString = String(marks)
+console.log(toString)
+console.log(typeof toString);
+
+//2)string to number
+let myName="Suyash"
+console.log(myName)
+console.log(typeof myName);
+
+let toNumber = Number(myName)
+console.log(toNumber)
+console.log(typeof toNumber);
+*/
+
+let isPass=undefined
+console.log(isPass)
+console.log(typeof isPass)
+
+let toString=String(isPass)
+console.log(toString)
+console.log(typeof toString);
+
+
+
+
+
