ecfc8f62493c9e92f579ef9407c8c314a7367ceb
SuyashS1 <sikchisuyash1@gmail.com>
2025-03-31 11:46:45 +0000

    datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use sctrict" //use all JS code as newer version
+
+//alert(3+3) // We are using nodejs and not the browser
+
+//Integer 
+//String
+//Boolean
+//BigInt
+//Null ==> standalone value
+//Undefined
+//Symbol
+
+const id=43
+let myName="Suyash"
+let isPass=true
+let registration_no=9007199254740991n
+let attendance=null;
+let year
+const division=Symbol()
+
+console.table([{id,myName,isPass,registration_no,attendance,year,division}])
+console.log(typeof id)
+console.log(typeof myName)
+console.log(typeof isPass)
+console.log(typeof registration_no)
+console.log(typeof attendance)
+console.log(typeof year)
+console.log(typeof division)
+
