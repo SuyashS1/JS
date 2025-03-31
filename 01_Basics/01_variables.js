8bcc513296c0300e276e7ef2da553adac157e7fb
SuyashS1 <sikchisuyash1@gmail.com>
2025-03-31 11:16:28 +0000

    learning variables

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,18 @@
+var myName="yash"
+
+let place="Pune"
+
+const id=53
+
+year="3rd year"
+
+console.log(myName)
+console.log(place)
+console.log(id)
+console.log(year)
+
+var myName="Suyash"
+place="Sonai"
+year="4th year"
+
+console.table([{myName,place,id,year}])
