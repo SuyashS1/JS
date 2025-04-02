2868a8eba409e39baef48c7ae41998fed48e30fd
SuyashS1 <sikchisuyash1@gmail.com>
2025-04-02 06:56:14 +0000

    String

---
+++ 01_Basics/04_strings.js
@@ -0,0 +1,31 @@
+// String --> Sequence/Collection of characters.
+
+let myName="Suyash"
+let lastName="Sikchi"
+
+console.log(`My name is ${myName} and my last name is ${lastName}, so you can say my full name is ${myName+lastName}`); 
+
+const firstName = new String('Suyash')
+console.log(firstName)
+
+console.log(myName.length)
+console.log(myName[4])
+
+console.log('My name is',myName, 'and my last name is ',lastName,'so you can say my full name is ',myName+lastName) 
+
+// \n = new line
+// \t = tab space
+
+//Methods 
+/*
+1)toUpperCase
+2)toLowerCase
+3)trim 
+4)concat 
+5)slice 
+6)indexof
+7)charAt 
+8)replace
+9)substring
+10)include
+*/
