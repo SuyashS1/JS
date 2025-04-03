cf248b10973d12053942b4eee892ecdc87f469fb
SuyashS1 <sikchisuyash1@gmail.com>
2025-04-03 14:26:14 +0000

    date and time

--- 01_Basics/06_date.js
+++ 01_Basics/06_date.js
@@ -1,3 +1,19 @@
-let Value=new Date("June 20, 69 00:20:18")
+//Date
 
-console.log(Value.g)
+let Value=new Date("01-24-2003")
+
+console.log(Value.toDateString())
+console.log(new Date().toString())
+console.log(new Date().toDateString())
+console.log(new Date().toISOString())
+console.log(new Date().toJSON())
+console.log(new Date().toLocaleDateString())
+console.log(new Date().toTimeString())
+console.log(new Date().toLocaleString())
+console.log(Value.getDay())
+
+//Time
+
+let myTime=Date.now()
+console.log(myTime)
+console.log(Value.getTime()) 
