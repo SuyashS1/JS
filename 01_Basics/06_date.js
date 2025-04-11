efb993836a79d1a6406647bd8c8fd7dd9170fd36
SuyashS1 <sikchisuyash1@gmail.com>
2025-04-11 05:47:53 +0000

    date and time

--- 01_Basics/06_date.js
+++ 01_Basics/06_date.js
@@ -1,5 +1,5 @@
 //Date
-
+/*
 let Value=new Date("01-24-2003")
 
 console.log(Value.toDateString())
@@ -12,8 +12,44 @@ console.log(new Date().toTimeString())
 console.log(new Date().toLocaleString())
 console.log(Value.getDay())
 
+const oldDate=new Date("2003-09-14T00:47:15Z")
+const date=new Date(2003, 8, 14, 0, 47, 15, 25)
+const mili=new Date(10000000000)
+
+console.log(oldDate)
+console.log(date)
+console.log(mili)
+*/
+
+const toGet=new Date()
+const toSet=new Date()
+
+console.log(toGet.getFullYear())
+console.log(toGet.getMonth())
+console.log(toGet.getDate())
+console.log(toGet.getHours())
+console.log(toGet.getMinutes())
+console.log(toGet.getSeconds())
+console.log(toGet.getMilliseconds())
+console.log(toGet.getDay())
+
+toSet.setFullYear(2003)
+toSet.setMonth(8)
+toSet.setDate(14)
+toSet.setHours(0)
+toSet.setMinutes(47)
+toSet.setSeconds(15)
+toSet.setMilliseconds(25)
+
+console.log(toSet)
+
+
+
+
+
 //Time
 
-let myTime=Date.now()
-console.log(myTime)
-console.log(Value.getTime()) 
+
+
+
+
